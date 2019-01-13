34883b2be6aeeb7ecfb98b1ed7091b95aa54b26d
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-13 21:55:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,8 @@ class Triangle
       :equilateral
     elsif @one == @two || @two == @three || @one == @three
       :isosceles
+    elsif @one != @two && @two != @three && @one != @three
+      :scalene
     end
   end
 
